b4a2dbc5c7ded753662e8f18071586d054641c85
EunSeok <dldmstjr114@naver.com>
2025-04-04 23:14:23 +0900

    한입리액트 저장

--- src/components/Exam.jsx
+++ src/components/Exam.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
+// reducer : 변환기
+// -> 상태를 실제로 변환시키는 변환기 역할
+function reducer(state, action){
+  console.log('state: ',state,'action: ',action);
+  // if (action.type === "INCREASE") {
+  //   return state + action.data
+  // }else if (action.type === "DECREASE") {
+  //   return state - action.data
+  // }  if 문을 사용해도 되지만 보통 switch문을 사용한다.
+  switch (action.type) {
+    case "INCREASE" : return state + action.data;
+    case "DECREASE" : return state - action.data;
+    default : return state;
+  }
+  
+}
 const Exam = () => {
+  // dispatch 발송하다, 급송하다.
+  // -> 상태변화가 있어야 한다는 사실을 알이는, 발송하는 함수
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  const onClickPlus = ()=>{
+    // 인수: 상태가 어떻게 변화되길 원하는지 
+    // -> 액션 객체
+    dispatch({
+      type : "INCREASE",  // 상태를 어떻게 변화시킬 원하는지 작성
+      data : 1
+    })
+  }
+  const onClickMinus = ()=>{
+    dispatch({
+      type : "DECREASE",
+      data : 1
+    })
+  }
+
   return (
     <div>
-      Exam
+      <h1>{state}</h1>
+      <button onClick={onClickPlus}>+</button>
+      <button onClick={onClickMinus}>-</button>
     </div>
   );
 };
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,26 +1,40 @@
-import React, { useState } from 'react';
-import './List.css'
-import TodoItem from './TodoItem';
-const List = ({todos,onUpdate,onDelete}) => {
-  const [search,setSearch] = useState('');
+import React, { useState } from "react";
+import "./List.css";
+import TodoItem from "./TodoItem";
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
   const onchangeSearch = (e) => {
     setSearch(e.target.value);
-  }
-  const getFilteredData = ()=>{
-    if(search === ""){
+  };
+  const getFilteredData = () => {
+    if (search === "") {
       return todos;
     }
-    return todos.filter((todo)=> todo.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+    return todos.filter((todo) =>
+      todo.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    );
   };
 
   const filteredTodos = getFilteredData();
   return (
-    <div className='List'>
+    <div className="List">
       <h4>Todo List ✏️</h4>
-      <input value={search} onChange={onchangeSearch} type="text" placeholder='검색어를 입력하세요' />
-      <div className='todo_wrapper'>
+      <input
+        value={search}
+        onChange={onchangeSearch}
+        type="text"
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todo_wrapper">
         {filteredTodos.map((todo) => {
-          return <TodoItem key={todo.id} {...todo } onUpdate={onUpdate}onDelete={onDelete}/>
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
         })}
       </div>
     </div>
